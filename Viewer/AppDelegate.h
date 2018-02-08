2085fdbfa5ad2fe309314781090c139b52802af9
Nguyen Quang Hung <Fuzitsu@Nguyens-Mac.local>
2018-02-08 08:54:44 +0700

    Initial Commit

---
+++ Viewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Viewer
+//
+//  Created by Nguyen Quang Hung on 2/8/18.
+//  Copyright (c) 2018 Nguyen Quang Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Viewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Viewer
+//
+//  Created by Nguyen Quang Hung on 2/8/18.
+//  Copyright (c) 2018 Nguyen Quang Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
